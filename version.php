c515b3aced894f416d6193f39a62294027312df3
Hittesh Ahuja <hittesh@thecrewacademy.com>
2022-10-03 14:32:44 +0100

    Bumped up version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'tool_certificate';
 $plugin->release      = '4.0.3';
-$plugin->version      = 2022082400;
+$plugin->version      = 2022082401;
 $plugin->requires     = 2022041900.00;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->supported    = [400, 400];
